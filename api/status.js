d549602f4d3ed0a67e0b54a3b21cb1bec3fdae11
aka <khaliqarrasyidabdul@gmail.com>
2026-03-30 20:07:09 +0700

    Create api/status.js

---
+++ api/status.js
@@ -0,0 +1,34 @@
+import crypto from "crypto"
+
+export default async function handler(req,res){
+
+  const {orderId} = req.body
+
+  const merchantCode="DS29215"
+  const apiKey="79fbf35e6a735c573fc56cfa8dc25be8"
+
+  const signature = crypto.createHash("md5")
+  .update(merchantCode + orderId + apiKey)
+  .digest("hex")
+
+  const payload={
+    merchantCode,
+    merchantOrderId:orderId,
+    signature
+  }
+
+  const r = await fetch("https://sandbox.duitku.com/webapi/api/merchant/transactionStatus",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify(payload)
+  })
+
+  const data = await r.json()
+
+  let status="PENDING"
+
+  if(data.statusCode==="00") status="SUCCESS"
+  if(data.statusCode==="02") status="CANCELED"
+
+  res.json({status,data})
+}
