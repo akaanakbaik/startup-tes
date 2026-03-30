16cd41ca53f61f834e441e40914aaf4e53cc51e7
aka <khaliqarrasyidabdul@gmail.com>
2026-03-30 18:44:38 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
 export const CONFIG = {
-  duitku: {
-    merchantCode: "DS29215",
-    apiKey: "79fbf35e6a735c573fc56cfa8dc25be8",
-    callbackUrl: "https://store.domku.xyz/callback",
-    returnUrl: "https://store.domku.xyz"
-  },
   api: "/api/duitku-create"
 }
