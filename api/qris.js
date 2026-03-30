b454e8d4813e1332bbab561078c0d1b1d2ef2573
aka <khaliqarrasyidabdul@gmail.com>
2026-03-30 20:06:48 +0700

    Create api/qris.js

---
+++ api/qris.js
@@ -0,0 +1,38 @@
+import crypto from "crypto"
+
+export default async function handler(req,res){
+
+  const merchantCode="DS29215"
+  const apiKey="79fbf35e6a735c573fc56cfa8dc25be8"
+
+  const orderId="ORD"+Date.now()
+  const amount=40000
+
+  const signature=crypto.createHash("md5")
+  .update(merchantCode+orderId+amount+apiKey)
+  .digest("hex")
+
+  const payload={
+    merchantCode,
+    paymentAmount:amount,
+    paymentMethod:"SP",
+    merchantOrderId:orderId,
+    productDetails:"QRIS TEST",
+    email:"test@test.com",
+    customerVaName:"AKADEV STORE",
+    callbackUrl:"https://store.domku.xyz/api/callback",
+    returnUrl:"https://store.domku.xyz/return",
+    signature,
+    expiryPeriod:10
+  }
+
+  const r=await fetch("https://sandbox.duitku.com/webapi/api/merchant/v2/inquiry",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify(payload)
+  })
+
+  const data=await r.json()
+
+  res.json(data)
+}
