ba4a34d4f52c41d11d8f11431b099b92d97dad9b
aka <khaliqarrasyidabdul@gmail.com>
2026-03-30 20:03:29 +0700

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -5,15 +5,15 @@ import QR from "./page/qr"
 import CC from "./page/creditcard"
 import Dana from "./page/ewalletdana"
 
-export default function Router() {
-  return (
+export default function Router(){
+  return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Utama />} />
-        <Route path="/pilih" element={<Pilih />} />
-        <Route path="/qris" element={<QR />} />
-        <Route path="/cc" element={<CC />} />
-        <Route path="/dana" element={<Dana />} />
+        <Route path="/" element={<Utama/>}/>
+        <Route path="/pilih" element={<Pilih/>}/>
+        <Route path="/qris" element={<QR/>}/>
+        <Route path="/cc" element={<CC/>}/>
+        <Route path="/dana" element={<Dana/>}/>
       </Routes>
     </BrowserRouter>
   )
