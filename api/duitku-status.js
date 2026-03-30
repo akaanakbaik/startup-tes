0797efb148b8dc51d537fdeb675266f24880b2df
aka <khaliqarrasyidabdul@gmail.com>
2026-03-30 19:16:47 +0700

    Create api/duitku-status.js

---
+++ api/duitku-status.js
@@ -0,0 +1,31 @@
+import crypto from "crypto"
+
+export default async function handler(req, res) {
+  try {
+    const merchantCode = "DS29215"
+    const apiKey = "79fbf35e6a735c573fc56cfa8dc25be8"
+
+    const { merchantOrderId } = req.body
+
+    const signature = crypto
+      .createHash("md5")
+      .update(merchantCode + merchantOrderId + apiKey)
+      .digest("hex")
+
+    const duitkuRes = await fetch("https://sandbox.duitku.com/webapi/api/merchant/transactionStatus", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        merchantCode,
+        merchantOrderId,
+        signature
+      })
+    })
+
+    const data = await duitkuRes.json()
+    return res.status(200).json(data)
+
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+}
